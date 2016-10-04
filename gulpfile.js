2955b5aa99f9c29d7454dba1c94f37bd86f88df2
MuriloDAG <dag.murilo@gmail.com>
2016-10-03 23:19:22 -0300

    Move dependencies to dev column and add gulp support

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,87 @@
-var gulp = require('gulp');
-var mocha = require('gulp-mocha');
-var util = require('gulp-util');
+const gulp = require('gulp');
 
-gulp.task('default', function() {
-	console.log("Gulp placeholder is working");
+/* testing */
+const mocha = require('gulp-mocha');
+const util = require('gulp-util');
+
+/* convert from es6 to es5 */
+const
+	browserify = require('browserify'),
+	babelify   = require('babelify'),
+	source     = require('vinyl-source-stream'),
+	buffer     = require('vinyl-buffer'),
+	browser    = require('browser-sync'),
+	sass       = require('gulp-sass'),
+	clean      = require('gulp-clean'),
+	uglify     = require('gulp-uglify'),
+	sourcemaps = require('gulp-sourcemaps');
+
+/*
+* Delete the contents of the build directory
+*/
+gulp.task('clean', function() {
+	gulp.src('build', {read: false})
+		.pipe(clean());
 });
 
-gulp.task('test', function () {
+/*
+ * Transpile es6 code to es5 using Babel and browserify
+ */
+gulp.task('transpile', function() {
+	return browserify('src/js/script.js', { debug : true })
+		.transform('babelify', {presets: ['es2015']})
+		.bundle()
+		.on('error', util.log.bind(util, 'Browserify Error'))
+		.pipe(source('main.js'))
+		.pipe(buffer())
+		.pipe(sourcemaps.init({ loadMaps: true }))
+		//.pipe(uglify())
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('./src/build'))
+		.pipe(browser.stream());
+});
+
+/*
+ * Start a server from buid directory
+ */
+gulp.task('serve', function() {
+	browser({
+		server: {
+			baseDir: './src'
+		}
+	});
+});
+
+/*
+ * Convert SASS files to CSS
+ */
+gulp.task('sass', function() {
+	return gulp.src('src/sass/style.scss')
+		.pipe(sass({
+			style: 'compressed'
+		}).on('error', sass.logError))
+		.pipe(gulp.dest('src/build'))
+		.pipe(browser.stream());
+});
+
+/*
+ * Watch for when JS, HTML, or SCSS files change so they can be updated
+ */
+gulp.task('watch', function() {
+	gulp.watch('src/js/**/*.js', ['transpile']);
+	gulp.watch('src/**/*.html', []);
+	gulp.watch('src/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('build', ['clean', 'sass', 'transpile']);
+gulp.task('default', ['build', 'serve', 'watch']);
+
+gulp.task('test', () => {
 	return gulp.src(['test/**/*.js'], { read: false })
 		.pipe(mocha({ reporter: 'spec' }))
 		.on('error', util.log);
 });
 
-gulp.task('watch-test', function () {
+gulp.task('watch-test', () => {
 	gulp.watch(['test/**'], ['test']);
 });
