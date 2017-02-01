d20bf9564e09b51fa3282b85e0aaf52198676bb1
Murilo Dell Agnolo Garcia <dag.murilo@gmail.com>
2017-02-01 02:50:41 -0200

    Add loading behaviour for universities and majors

--- src/pages/major-list/major-list.ts
+++ src/pages/major-list/major-list.ts
@@ -9,54 +9,44 @@ import { Major } from '../../models/major';
 import { DatabaseService } from '../../services/database-service';
 import { StateService } from '../../services/state-service';
 
+@Wove({ debug: true })
 @Component({
 	selector: 'major-list',
 	templateUrl: 'major-list.html'
 })
 export class MajorListPage {
-	selectedUni: Uni;
-	noMajorsAvailable: boolean = false;
-	majors: Major[];
+	get selectedUni(): Uni {
+		return this.navParams.get('uni');
+	};
 
-	constructor(private dbService: DatabaseService, private stateService: StateService, public navCtrl: NavController, public navParams: NavParams) {
-		console.debug("major-list:constructor()");
+	loading: boolean = true;
+	majors: Major[] = [];
+
+	constructor(
+		private dbService: DatabaseService,
+		private stateService: StateService,
+		private navCtrl: NavController,
+		private navParams: NavParams) {
 	}
 
 	ionViewWillEnter() {
-		console.debug("major-list:ionViewWillEnter()");
-
-		// If we navigated to this page, we will have an uni available as a nav param
-		this.selectedUni = this.navParams.get('uni');
-		console.debug("major-list:ionViewWillEnter() - uni", this.selectedUni);
-
 		const previouslySelectedMajor = this.stateService.getCurrentMajor();
-		console.debug('major-list:ionViewWillEnter() - old major', previouslySelectedMajor);
 
 		if (previouslySelectedMajor) {
-			console.debug('major-list:ionViewWillEnter() - show units for saved major');
 			this.majorSelected(previouslySelectedMajor);
 		} else {
-			// prevent reloading data from firebase
-			if (!this.majors) {
-				console.debug('major-list:ionViewWillEnter() - get majors from server');
-				this.dbService.getMajors(this.selectedUni).subscribe(majors => {
-					this.majors = majors;
-					console.debug("major-list:ionViewWillEnter() - majors", this.majors);
-
-					this.noMajorsAvailable = majors.length <= 0;
-					console.debug("major-list:ionViewWillEnter() - noMajorsAvailable", this.noMajorsAvailable);
-				});
-			}
+			this.dbService.getMajors(this.selectedUni).subscribe(majors => {
+				this.majors = majors;
+				this.loading = false
+			});
 		}
 	};
 
 	backToUniList() {
-		console.log('major-list:backToUniList()');
 		this.stateService.setCurrentUni(null);
-	}
+	};
 
 	majorSelected(major) {
-		console.debug('major-list:majorSelected()', major);
 		this.stateService.setCurrentMajor(major);
 		this.navCtrl.push(UnitListPage, {
 			uni: this.selectedUni,
--- src/pages/uni-list/uni-list.ts
+++ src/pages/uni-list/uni-list.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Wove } from 'aspect.js';
 
 import { MajorListPage } from '../major-list/major-list';
 import { Uni } from '../../models/uni';
@@ -7,43 +8,39 @@ import { Uni } from '../../models/uni';
 import { DatabaseService } from '../../services/database-service';
 import { StateService } from '../../services/state-service';
 
+@Wove({ debug: true })
 @Component({
 	selector: 'uni-list',
 	templateUrl: 'uni-list.html'
 })
-
 export class UniListPage {
+	loading: boolean = true;
 	unis: Uni[];
 
-	constructor(private dbService: DatabaseService, private stateService: StateService, public navCtrl: NavController, public navParams: NavParams) {
-		console.debug('uni-list:constructor()');
-	}
+	constructor(
+		private dbService: DatabaseService,
+		private stateService: StateService,
+		private navCtrl: NavController,
+		private navParams: NavParams) {
+	};
 
 	ionViewWillEnter() {
-		console.debug('uni-list:ionViewWillEnter()');
-
 		const previouslySelectedUni = this.stateService.getCurrentUni();
-		console.debug('uni-list:ionViewWillEnter() - old uni', previouslySelectedUni);
 
 		if (previouslySelectedUni) {
-			console.debug('uni-list:ionViewWillEnter() - show majors for saved uni');
 			this.uniSelected(previouslySelectedUni);
 		} else {
-			// prevent reloading data from firebase
-			if (!this.unis) {
-				console.debug('uni-list:ionViewWillEnter() - get unis from server');
-				this.dbService.getUnis().subscribe(
-					unis => this.unis = unis
-				);
-			}
+			this.dbService.getUnis().subscribe(unis => {
+				this.unis = unis
+				this.loading = false;
+			});
 		}
-	}
+	};
 
 	uniSelected(uni) {
-		console.debug('uni-list:uniSelected()', uni);
 		this.stateService.setCurrentUni(uni);
 		this.navCtrl.push(MajorListPage, {
 			uni: uni,
 		});
-	}
+	};
 }
