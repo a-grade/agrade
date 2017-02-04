b51a211ea92d5b5518759ccbb4a8d08069b7743b
Murilo Dell Agnolo Garcia <dag.murilo@gmail.com>
2017-02-03 22:20:45 -0200

    Add new properties to unit model

--- src/models/unit.ts
+++ src/models/unit.ts
@@ -2,10 +2,8 @@
 export class Unit {
 	$key: string;
 	name: string;
-	period: string;
-	constructor(key: string, name: string, period: string) {
-		this.$key = key;
-		this.name = name;
-		this.period = period;
-	};
+	type: string;
+	condition: string;
+	tags: string[];
+	value: number;
 }
